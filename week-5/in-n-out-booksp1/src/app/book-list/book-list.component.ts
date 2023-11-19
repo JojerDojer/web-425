f671e9ef2c39cb2033e77ecf79123c096180950b
Jojo <davidson.jojo@yahoo.com>
2023-11-19 14:43:02 -0600

    fixed issue with in-n-out-booksp1

---
+++ week-5/in-n-out-booksp1/src/app/book-list/book-list.component.ts
@@ -0,0 +1,23 @@
+/**
+ * Title: book-list.component.ts
+ * Author: John Davidson
+ * Date: 19 November 2023
+ * Description: Book-list component
+ */
+
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss']
+})
+export class BookListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
